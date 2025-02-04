e6cfe55a44a309ca6472e94ce0dc4c45ee795df6
SK-Merokhel <sufyanmerokhel@gmail.com>
2025-02-04 23:09:45 +0500

    update

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/mnt/sda5/Marketplace-Hackathon-3/src/app/page\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"],\"/mnt/sda5/Marketplace-Hackathon-3/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
